990ce17007387a22a6f65dbd14cda3ada0adb7af
Fernando <juanfdoa@hotmail.com>
2021-08-25 12:11:18 -0500

    Conversor de grados

---
+++ seccion 3/App.js
@@ -0,0 +1,18 @@
+
+
+console.log('Hola Fernando');
+
+
+let c =0;
+let f = 90;
+c = (f - 32);
+c = c * (5 / 9);
+console.log('El resultado de convertir 90° Fahrenheit a Celsius es');
+console.log(c);
+
+
+let C = 90;
+let K = 0;
+K = C + 273.15;
+console.log('El resultado de convertir 90° Celsius a Kelvin es');
+console.log(K);
