a4dddcbacf9fc6ff3aef30fdba8b39901ea599bc
Fernando <juanfdoa@hotmail.com>
2021-08-26 11:53:17 -0500

    Ciclos (while and for)

---
+++ seccion 4.4/App.js
@@ -0,0 +1,10 @@
+
+
+let Gasolina = 50;
+
+while (Gasolina > 0) {
+    console.log('La gasolina restante es ' + Gasolina);
+    Gasolina--;
+}
+console.log('Ya no tienes gasolina');
+
---
+++ seccion 4.5/App.js
@@ -0,0 +1,13 @@
+// imprimir la tabal del 10
+// definir un limite de la tabla
+
+let base = 10;
+let limite = 20;
+
+for (let i = 1; i <= limite; i++) {
+    resultado = base * i;
+
+    console.log(base + ' x ' + i + ' = ' + resultado);
+    
+    
+}
