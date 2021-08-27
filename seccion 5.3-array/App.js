2e70182554bdc392cc6a04761e803caa0a8fb712
Fernando <juanfdoa@hotmail.com>
2021-08-27 13:02:12 -0500

    Arreglos (arrays)

---
+++ seccion 5.3-array/App.js
@@ -0,0 +1,8 @@
+
+
+let flores = ['Rosa', 'Girasol', 'Lirio'];
+
+
+console.log(flores[0]);
+
+
---
+++ seccion 5.4/App.js
@@ -0,0 +1,12 @@
+
+
+let personas = ['Luis' , 'Carlos', 'Camilo'];
+let salarios = [1000 , 1200 , 3000];
+
+for(let i = 0; i <= 2 ; i++){
+
+    console.log(personas[i] + ' tiene un salario de: ' + salarios[i]);
+    
+   
+}
+
